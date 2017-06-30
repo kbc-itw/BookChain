c22436004db2f97f5d7f289581ba3bdbfcfa2f9c
Huruikagi <kbc14a04@stu.kawahara.ac.jp>
2017-06-30 18:03:48 +0900

    webpackで開発環境用ビルドと本番環境用ビルドを行えるようにする

--- web-client/webpack.config.ts
+++ web-client/webpack.config.ts
@@ -1,28 +1,80 @@
-import {Configuration} from "webpack";
+import {Configuration, Entry, Output, Plugin, optimize} from "webpack";
 import * as path from "path";
 
 
+// 環境変数 `NODE_ENV` が本番環境用であるか否か。
+const inProduction: boolean = process.env.NODE_ENV === "production";
 
+
+// バンドルする対象のエントリポイント
+function entry(): Entry {
+    if (inProduction) {
+        // 本番環境
+        return {
+            app: "./src/index.ts"
+        };
+    } else {
+        // 開発環境
+        // 通常のindexに加えて、テスト用コードもコンパイル・バンドル
+        return {
+            app: "./src/index.ts",
+            test: "./test/test-index.ts"
+        };
+    }
+}
+
+
+// バンドルした結果を、どこにどういう名前で出力するか
+function output(): Output {
+    if (inProduction) {
+        return {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].bundle.min.js'
+        };
+    } else {
+        return {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].bundle.js'
+        };
+    }
+}
+
+
+// デバッグ強化開発者用ツール
+// ソースマップを出力するか否か。
+function devTool(): "source-map" | boolean {
+    if (inProduction) {
+        return false;
+    } else {
+        return "source-map";
+    }
+}
+
+
+// webpack プラグイン
+function plugins(): Plugin[] {
+    if (inProduction) {
+        // 本番環境用では、jsファイルの最小化を行う
+        return [
+            new optimize.UglifyJsPlugin()
+        ];
+    } else {
+        // 開発環境だと（圧縮処理が重いので）なし
+        return [];
+    }
+}
 
 
 /**
- * webpack用の設定ファイル。
+ * webpack用の設定オブジェクト
  */
 const config: Configuration = {
-
-    entry: './test/test-index.ts',
-
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: './test-bundle.js'
-    },
-
-    devtool: 'source-map',
-
+    entry: entry(),
+    output: output(),
+    devtool: devTool(),
     resolve: {
         extensions: ['.ts', '.tsx', '.js']
     },
-
     module: {
         rules: [
             {
@@ -30,7 +82,8 @@ const config: Configuration = {
                 loader: 'ts-loader'
             }
         ]
-    }
+    },
+    plugins: plugins()
 };
 
 
