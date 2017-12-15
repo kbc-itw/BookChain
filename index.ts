364fdfdeeb2c6e2cee8d83ba61280f18f83fef4e
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-12-15 12:18:08 +0900

    エントリポイントがbootstrap関数を利用するように

--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
 import * as express from 'express';
 import * as smp from 'source-map-support';
+import { bootstrap } from './app/bootstrap';
 
 smp.install();
 
-const app = express();
-
-app.listen(80, () => {
+bootstrap().listen(80, () => {
     console.log('Example app listening on port 80!');
 });
