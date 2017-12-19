a8aab79f0805df9f06a86fe001d085cdbe088ec1
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-12-19 20:23:16 +0900

    部屋の目的ならびにinviter用のエラーメッセージをenumに追加

--- app/messages.ts
+++ app/messages.ts
@@ -5,8 +5,12 @@ export enum ErrorMessages {
     MESSAGE_OFFSET_INVALID = 'offsetには0以上の整数を指定します',
     MESSAGE_HOST_INVALID = 'ホスト名にはFQDNないしlocalhostを指定します',
     MESSAGE_LOCAL_ID_INVALID = 'ローカルIDにはIDを指定します',
+    MESSAGE_ROOM_PURPOSE_INVALID = 'purposeにはrentalかreturnを指定します',
+    MESSAGE_INVITER_INVALID = 'inviterにはlocatorを指定します',
     MESSAGE_OWNER_REQUIRED = 'ownerは必須です',
     MESSAGE_ISBN_REQUIRED = 'isbnは必須です',
     MESSAGE_HOST_REQUIRED = 'ホスト名は必須です',
     MESSAGE_LOCAL_ID_REQUIRED = 'ローカルIDは必須です',
+    MESSAGE_ROOM_PURPOSE_REQUIRED = 'purposeは必須です',
+    MESSAGE_INVITER_REQUIRED = 'inviterは必須です',
 }
