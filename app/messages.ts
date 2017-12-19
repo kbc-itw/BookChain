d6b983485726ffcf6ce2f138f713a188d6bb0235
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-12-19 17:44:50 +0900

    クライアントに送信するエラーメッセージを定数化。

---
+++ app/messages.ts
@@ -0,0 +1,11 @@
+export const MESSAGE_OWNER_INVALID = 'ownerにはlocatorを指定します';
+export const MESSAGE_ISBN_INVALID = 'isbnには13桁のisbnを指定します';
+export const MESSAGE_LIMIT_INVALID = 'limitには0より大きい整数を指定します';
+export const MESSAGE_OFFSET_INVALID = 'offsetには0以上の整数を指定します';
+export const MESSAGE_HOST_INVALID = 'ホスト名にはFQDNないしlocalhostを指定します';
+export const MESSAGE_LOCAL_ID_INVALID = 'ローカルIDにはIDを指定します';
+export const MESSAGE_OWNER_REQUIRED = 'ownerは必須です';
+export const MESSAGE_ISBN_REQUIRED = 'isbnは必須です';
+export const MESSAGE_HOST_REQUIRED = 'ホスト名は必須です';
+export const MESSAGE_LOCAL_ID_REQUIRED = 'ローカルIDは必須です';
+
