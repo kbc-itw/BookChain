fb4c1c52af416361418c065e5d195613ae9de7f4
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2017-12-19 18:36:53 +0900

    エラーメッセージ用enumにisreturnedとborrower関連の要素追加

--- app/messages.ts
+++ app/messages.ts
@@ -1,6 +1,8 @@
 export enum ErrorMessages {
     MESSAGE_OWNER_INVALID = 'ownerにはlocatorを指定します',
+    MESSAGE_BORROWER_INVALID = 'borrowerにはlocatorを指定します',
     MESSAGE_ISBN_INVALID = 'isbnには13桁のisbnを指定します',
+    MESSAGE_ISRETURNED_INVALID = 'isReturnedにはtrueかfalseを指定します',
     MESSAGE_LIMIT_INVALID = 'limitには0より大きい整数を指定します',
     MESSAGE_OFFSET_INVALID = 'offsetには0以上の整数を指定します',
     MESSAGE_HOST_INVALID = 'ホスト名にはFQDNないしlocalhostを指定します',
