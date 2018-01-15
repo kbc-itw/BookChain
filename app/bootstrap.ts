6b6fd4254408b1b97770ce139313b9ad19487bba
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2018-01-15 15:33:26 +0900

    bootstrapで認証関連の設定を行うように変更

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -4,8 +4,12 @@ import { chainCodeQuery, chainCodeInvoke } from './chaincode-connection';
 import { createUserRouter } from './router/user';
 import { logger } from './logger';
 import * as bodyParser from 'body-parser';
-import { passport } from './authenticator';
+import { passport, isAuthenticated } from './authenticator';
+import { Request, Response } from 'express-serve-static-core';
+import { createAuthenticationRouter } from './router/authenticate';
 
+const session = require('express-session');
+const secret = require('../config/secrets.json').session_secret;
 /**
  * listenで起動可能なexpressアプリケーションを返す。
  * 呼び出しごとに別のアプリケーションを生成することに注意。
@@ -20,13 +24,14 @@ export function bootstrap():express.Application {
 }
 
 function configureRoute(app: express.Application) {
-//    app.use('/user', createUserRouter(chainCodeQuery, chainCodeInvoke));
+    //    app.use('/user', createUserRouter(chainCodeQuery, chainCodeInvoke));
 }
 
-function configureUse(app: express.Application) {
+export function configureUse(app: express.Application) {
     app.use(bodyParser.urlencoded({
         extended: true,
     }));
+    app.use(session({ secret }));
     app.use(bodyParser.json());
     app.use(passport.initialize());
     app.use(passport.session());
