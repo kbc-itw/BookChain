1bf0b956c52a3b9010f20cbf76aa6d001493bbb1
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2018-01-15 11:56:35 +0900

    bootstrapでpassportを指定

--- app/bootstrap.ts
+++ app/bootstrap.ts
@@ -4,6 +4,8 @@ import { chainCodeQuery, chainCodeInvoke } from './chaincode-connection';
 import { createUserRouter } from './router/user';
 import { logger } from './logger';
 import * as bodyParser from 'body-parser';
+import { passport } from './authenticator';
+
 /**
  * listenで起動可能なexpressアプリケーションを返す。
  * 呼び出しごとに別のアプリケーションを生成することに注意。
@@ -26,5 +28,7 @@ function configureUse(app: express.Application) {
         extended: true,
     }));
     app.use(bodyParser.json());
+    app.use(passport.initialize());
+    app.use(passport.session());
     // ミドルウェアをここに追加
 }
