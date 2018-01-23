09ca2b80e0e1e4b5b561ce0841f66afd4c5a09fd
t-nakamura <kbc14a08@stu.kawahara.ac.jp>
2018-01-23 16:33:25 +0900

    [clean] 不要なimportを削除
    # Conflicts:
    #	test/roomWebSocket.spec.ts

--- test/roomWebSocket.spec.ts
+++ test/roomWebSocket.spec.ts
@@ -1,21 +1,23 @@
 import 'mocha';
-import { expect } from 'chai';
+import * as chai from 'chai';
 import * as express from 'express';
 import * as config from 'config';
 import { IServerConfig } from '../app/config/IServerConfig';
-import { createTradingsRouter } from '../app/router/tradings';
-import{ configureUse } from '../app/bootstrap';
-import { Server } from 'http';
-import { createWebSocketServer, SocketRoom } from './../app/roomWebSocket';
 import { isFQDN, isUUID, isRoleString, isLocator, isISBN, isRoomPurpose, UUID, FQDN, RoomPurpose, Locator, RoleString } from '../app/util';
-import { stringify } from 'querystring';
-import { String } from 'shelljs';
+import * as http from 'http';
+import * as ws from 'ws';
+import { IWebSocketConfig } from '../app/config/IWSConfig';
+import * as bodyParser from 'body-parser';
+import { createWebSocketServer, SocketRoom } from '../app/roomWebSocket';
 const uuidv4 = require('uuid/v4');
+chai.use(require('chai-as-promised'));
 
-
-describe('rental', () => {
-
-    // const { port, host } = serverConfig;
+describe('webSocket', () => {
+    let app: express.Express;
+    let server: http.Server;
+    let wss: ws.Server;
+    const serverConfig = config.get<IServerConfig>('server');
+    const { port, host } = serverConfig;
     const uuid: UUID = getUniqueStr();
     const tokenUUID: UUID = getUniqueStr();
     const fqdn = 'example.com';
@@ -25,7 +27,7 @@ describe('rental', () => {
     const isbn = '9784274068560';
     let socketRoom: SocketRoom;
     // socketRoomのデフォルトを設定
-    if (isFQDN(fqdn) && isLocator(inviter) && isRoomPurpose(roomPurposeRental) && isLocator(guest) && isISBN(isbn) ){
+    if (isFQDN(fqdn) && isLocator(inviter) && isRoomPurpose(roomPurposeRental) && isLocator(guest) && isISBN(isbn)) {
         socketRoom = {
             isbn,
             room: {
@@ -44,21 +46,44 @@ describe('rental', () => {
             guestSocket: undefined,
         };
     }
+    const wsConfig = config.get<IWebSocketConfig>('webSocket');
+    const wsPort = wsConfig.port;
+    const wsHost = wsConfig.host;
 
-    beforeEach(async () => {
+    beforeEach((done) => {
+        app = express();
+        app.use(bodyParser.urlencoded({
+            extended: true,
+        }));
+        app.use(bodyParser.json());
+        server = app.listen(port, host, () => {
+            createWebSocketServer(server, '/rooms/connect', new Map(), async () => {}, async () => {})
+                .then((result) => {
+                    wss = result;
+                    done();
+                });
+        });
     });
 
-    afterEach(async () => {
+    afterEach((done) => {
+        server.close(() => {
+            wss.close(() => {
+                done();
+            });
+        });
     });
 
+    it('借りる', async () => {
+        chai.expect(wss).not.to.be.undefined;
+    });
+
+    function getUniqueStr(): UUID {
+        const uuid = uuidv4();
+        if (isUUID(uuid)) {
+            return uuid;
+        }
+        throw new Error();
+    }
 
 });
 
-function getUniqueStr(): UUID {
-    const uuid = uuidv4();
-    if (isUUID(uuid)) {
-        return uuid;
-    }
-    throw new Error();
-
-}
