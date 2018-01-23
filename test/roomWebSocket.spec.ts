b89b5d6e661bd280af8f9495d5b0eea04f4d9247
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2018-01-23 15:22:02 +0900

    spec修正

--- test/roomWebSocket.spec.ts
+++ test/roomWebSocket.spec.ts
@@ -5,34 +5,47 @@ import * as config from 'config';
 import { IServerConfig } from '../app/config/IServerConfig';
 import { createTradingsRouter } from '../app/router/tradings';
 import{ configureUse } from '../app/bootstrap';
-import { createWebSocketServer, SocketRoom } from './../app/roomWebSocket';
+import { SocketRoom, configureWebSocket, createWebSocketServer } from './../app/roomWebSocket';
 import { isFQDN, isUUID, isRoleString, isLocator, isISBN, isRoomPurpose, UUID, FQDN, RoomPurpose, Locator, RoleString } from '../app/util';
 import { stringify } from 'querystring';
 import { String } from 'shelljs';
 import * as http from 'http';
 import * as ws from 'ws';
+import { IWebSocketConfig } from '../app/config/IWSConfig';
+import * as bodyParser from 'body-parser';
 const uuidv4 = require('uuid/v4');
 chai.use(require('chai-as-promised'));
 
 describe('webSocket', () => {
     let app: express.Express;
     let server: http.Server;
-    let appServer: http.Server;
     let wss: ws.Server;
-
     const serverConfig = config.get<IServerConfig>('server');
     const { port, host } = serverConfig;
+    const wsConfig = config.get<IWebSocketConfig>('webSocket');
+    const wsPort = wsConfig.port;
+    const wsHost = wsConfig.host;
 
     beforeEach((done) => {
-        server = http.createServer();
         app = express();
-        wss = new ws.Server({ port, host, server:appServer, path:'hoge/hoge' }, () => {appServer = app.listen(port); done();});
+        app.use(bodyParser.urlencoded({
+            extended: true,
+        }));
+        app.use(bodyParser.json());
+        server = app.listen(port, host, () => {
+            createWebSocketServer(server, '/rooms/connect', new Map(), async () => {}, async () => {})
+                .then((result) => {
+                    wss = result;
+                    done();
+                });
+        });
     });
 
     afterEach((done) => {
-        appServer.close();
-        wss.close(() => {
-            done();
+        server.close(() => {
+            wss.close(() => {
+                done();
+            });
         });
     });
 
