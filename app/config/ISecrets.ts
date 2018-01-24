19ccb995022bc1091410911e5e291f602dd31490
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2018-01-24 13:58:21 +0900

    readonlyとケツの改行ぬけ

--- app/config/ISecrets.ts
+++ app/config/ISecrets.ts
@@ -1,7 +1,7 @@
 export interface ISecrets {
     readonly SESSION_SECRET: string;
     readonly facebook: {
-        FACEBOOK_APP_ID: string;
-        FACEBOOK_APP_SECRET: string;
+        readonly FACEBOOK_APP_ID: string;
+        readonly FACEBOOK_APP_SECRET: string;
     };
 }
