800b2ce914ed0577d22129b58868acac82e1e996
Huruikagi <kbc14a04@stu.kawahara.ac.jp>
2018-02-13 15:02:56 +0900

    不要な記述を削除

--- app/authenticator.ts
+++ app/authenticator.ts
@@ -1,7 +1,6 @@
 import { Passport } from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 import { Strategy as FaceBookStrategy } from 'passport-facebook';
-import * as config from 'config';
 import { NextFunction } from 'express';
 import { Request, Response } from 'express-serve-static-core';
 import { ISecrets } from './config/ISecrets';
@@ -9,7 +8,6 @@ import { AuthDb, IUserAuth } from './auth-db';
 
 export const passport = new Passport();
 
-const domain = config.get<string>('domain');
 const secrets = require('../config/secrets.json') as ISecrets;
 
 const localStrategy = new LocalStrategy((username, password, done) => {
@@ -23,7 +21,7 @@ const localStrategy = new LocalStrategy((username, password, done) => {
 const facebookStrategy = new FaceBookStrategy({
     clientID: secrets.facebook.FACEBOOK_APP_ID,
     clientSecret: secrets.facebook.FACEBOOK_APP_SECRET,
-    callbackURL: domain + '/auth/facebook/callback',
+    callbackURL: '/auth/facebook/callback',
 },async (accessToken, refreshToken, profile, done) => {
     try {
         const auth: IUserAuth = await AuthDb.facebook.upsert(accessToken, refreshToken, profile);
