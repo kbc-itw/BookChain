d441c26cfa52f1af067cf4f246ee85c1c573e6de
kbc14a12 <kbc14a12@stu.kawahara.ac.jp>
2018-02-15 16:59:30 +0900

    JSON.stringifyするとマズいっぽいんで外すなり法隆寺

--- app/roomWebSocket.ts
+++ app/roomWebSocket.ts
@@ -29,7 +29,7 @@ export function createWebSocketServer(
                     // req.urlはundefinedの可能性がある
                     // パラメタ必須なので、なければソケットを閉じる
                     logger.info('不正なWebSocket接続 req.urlがundefined');
-                    await socket.send(JSON.stringify({ action: 'INVALID_ACTION', data: { youSend: { action: 'connction' }, message: 'req.url is undefined' } }));
+                    await socket.send({ action: 'INVALID_ACTION', data: { youSend: { action: 'connction' }, message: 'req.url is undefined' } });
                     socket.close();
                     return;
                 }
@@ -42,7 +42,7 @@ export function createWebSocketServer(
 
                 if (invalidField.size > 0) {
                     logger.info(`webSocket接続時の不正なパラメタ id:${id} role:${role} locator:${locator} inviteToken:${inviteToken}`);
-                    await socket.send(JSON.stringify({
+                    await socket.send({
                         action: 'INVALID_ACTION',
                         data: {
                             youSend: {
@@ -54,7 +54,7 @@ export function createWebSocketServer(
                             },
                             message: 'invalid parameters',
                         },
-                    }));
+                    });
                     socket.close();
                     return;
                 }
@@ -63,7 +63,7 @@ export function createWebSocketServer(
             
                 if (typeof room === 'undefined') {
                     logger.info(`未プールの部屋 ${id}`);
-                    await socket.send(JSON.stringify({
+                    await socket.send({
                         action: 'INVALID_ACTION',
                         data: {
                             youSend: {
@@ -71,14 +71,14 @@ export function createWebSocketServer(
                             },
                             message: 'room does not exist',
                         },
-                    }));
+                    });
                     socket.close();
                     return;
                 }
 
                 if (room.room.closedAt) {
                     logger.info(`既に閉じている部屋 ${id}`);
-                    await socket.send(JSON.stringify({
+                    await socket.send({
                         action: 'INVALID_ACTION',
                         data: {
                             youSend: {
@@ -86,7 +86,7 @@ export function createWebSocketServer(
                             },
                             message: 'room is already closed',
                         },
-                    }));
+                    });
                     socket.close();
                     return;
                 }
@@ -107,10 +107,10 @@ export function createWebSocketServer(
                             break;
                         case 'CANCEL_REQUEST':
                             if (room.guestSocket) {
-                                await room.guestSocket.send(JSON.stringify({
+                                await room.guestSocket.send({
                                     action: 'TRANSACTION_CANCELED',
                                     data: 'inviter canceled transaction',
-                                }));
+                                });
                             }
                             closeRoom(room, invokeFunction);
                             break;
@@ -120,7 +120,7 @@ export function createWebSocketServer(
                 } else {
                     if (room.inviteToken !== inviteToken) {
                         logger.info(`inviteToken不一致 ${inviteToken}`);
-                        await socket.send(JSON.stringify({
+                        await socket.send({
                             action: 'INVALID_ACTION',
                             data: {
                                 youSend: {
@@ -128,11 +128,11 @@ export function createWebSocketServer(
                                 },
                                 message: 'inviteToken does not match',
                             },
-                        }));
+                        });
                         if (room.inviterSocket) {
-                            await room.inviterSocket.send(JSON.stringify({
+                            await room.inviterSocket.send({
                                 action: 'GUEST_DISCONNECTED',
-                            }));
+                            });
                         }
                         await closeRoom(room, invokeFunction);
                         return;
@@ -140,12 +140,12 @@ export function createWebSocketServer(
 
                     if (typeof room.inviterSocket === 'undefined') {
                         logger.info(`inviterSocket存在せず id:${room.room.id}`);
-                        await socket.send(JSON.stringify({
+                        await socket.send({
                             action: 'INVALID_ACTION',
                             data: {
                                 message: 'invalid room',
                             },
-                        }));
+                        });
                         await closeRoom(room, invokeFunction);
                         return;
                     }
@@ -153,17 +153,17 @@ export function createWebSocketServer(
                     room.guestSocket = socket;
                     room.room.guest = locator;
 
-                    await socket.send(JSON.stringify({
+                    await socket.send({
                         action: 'ENTRY_PERMITTED',
                         data: {
                             ...room.room,
                         },
-                    }));
+                    });
 
-                    await room.inviterSocket.send(JSON.stringify({
+                    await room.inviterSocket.send({
                         action: 'USER_JOINED',
                         data: locator,
-                    }));
+                    });
 
                     try {
                         await invokeFunction({
@@ -187,7 +187,7 @@ export function createWebSocketServer(
                         switch (params.action) {
                         case 'REQUEST_PROPOSAL':
                             if (!params.data) {
-                                await socket.send(JSON.stringify({
+                                await socket.send({
                                     action: 'INVALID_ACTION',
                                     data: {
                                         youSend: {
@@ -195,11 +195,11 @@ export function createWebSocketServer(
                                         },
                                         message: 'data is required',
                                     },
-                                }));
+                                });
                                 await closeRoom(room, invokeFunction);
                                 break;
                             } else if (params.data && !isISBN(params.data)) {
-                                await socket.send(JSON.stringify({
+                                await socket.send({
                                     action: 'INVALID_ACTION',
                                     data: {
                                         youSend: {
@@ -207,7 +207,7 @@ export function createWebSocketServer(
                                         },
                                         message: 'data must be ISBN',
                                     },
-                                }));
+                                });
                                 await closeRoom(room, invokeFunction);
                                 break;
                             }
@@ -222,9 +222,9 @@ export function createWebSocketServer(
                             };
 
                             if (room.inviterSocket) {
-                                await room.inviterSocket.send(JSON.stringify(proposal));
+                                await room.inviterSocket.send(proposal);
                             }
-                            await socket.send(JSON.stringify(proposal));
+                            await socket.send(proposal);
                             break;
                         case 'APPROVE_PROPOSAL':
                             room.guestApproved = true;
@@ -234,10 +234,10 @@ export function createWebSocketServer(
                             break;
                         case 'CANCEL_REQUEST':
                             if (room.inviterSocket) {
-                                await room.inviterSocket.send(JSON.stringify({
+                                await room.inviterSocket.send({
                                     action: 'TRANSACTION_CANCELED',
                                     data: 'guest canceled transaction',
-                                }));
+                                });
                             }
                             closeRoom(room, invokeFunction);
                             break;
@@ -302,10 +302,10 @@ async function commitment(room: SocketRoom, queryFunction: (request: ChaincodeQu
         },
     };
     if (room.inviterSocket) {
-        await room.inviterSocket.send(JSON.stringify(commitment));
+        await room.inviterSocket.send(commitment);
     }
     if (room.guestSocket) {
-        await room.guestSocket.send(JSON.stringify(commitment));
+        await room.guestSocket.send(commitment);
     }
     closeRoom(room, invokeFunction);
 }
